73ce1122aa9fc12dfa5ec4bbe9f266da9aaa4162
niisan-tokyo <niikura23@gmail.com>
2022-10-02 18:30:45 +0900

    TypeScriptテストが落ちるまで

---
+++ TypeScript/FizzBuz.ts
@@ -0,0 +1,5 @@
+export class FizzBuzz {
+    public run(num: number): string {
+        return ''
+    }
+}
---
+++ TypeScript/__tests__/FizzBuzz.test.ts
@@ -0,0 +1,37 @@
+import { FizzBuzz } from '../FizzBuz'
+
+const obj = new FizzBuzz()
+
+describe('FizzBuzzのテスト', () => {
+    test('1をいれたら1を返す', () => {
+        expect(obj.run(1)).toBe('1')
+    })
+
+    test('2をいれたら2を返す', () => {
+        
+    })
+
+    test('3をいれたらFizzを返す', () => {
+        
+    })
+
+    test('6をいれたらFizzを返す', () => {
+        
+    })
+
+    test('5をいれたらBuzzを返す', () => {
+        
+    })
+
+    test('10をいれたらBuzzを返す', () => {
+        
+    })
+
+    test('15をいれたらFizzBuzzを返す', () => {
+        
+    })
+
+    test('30をいれたらFizzBuzzを返す', () => {
+        
+    })
+})
