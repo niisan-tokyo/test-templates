74f8e17d36fc42db246d5283272e14aea04144a4
niisan-tokyo <niikura23@gmail.com>
2022-10-02 17:14:32 +0900

    TODO作ってテスト失敗

---
+++ ruby/FizzBuzz.rb
@@ -0,0 +1,5 @@
+class FizzBuzz
+    def run(num)
+
+    end
+end
---
+++ ruby/FizzBuzzTest.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require './FizzBuzz'
+
+class FizzBuzzTest < Test::Unit::TestCase
+    def setup 
+        @obj = FizzBuzz.new
+    end
+
+    def test_1を入れたら1が返る
+        assert_equal 1, @obj.run(1)
+    end
+    def test_2を入れたら2が返る
+
+    end
+    def test_3を入れたらFizzが返る
+
+    end
+    def test_6を入れたらFizzが返る
+
+    end
+    def test_5を入れたらBuzzが返る
+
+    end
+    def test_10を入れたらBuzzが返る
+
+    end
+    def test_15を入れたらFizzBuzzが返る
+
+    end
+    def test_30を入れたらFizzBuzzが返る
+
+    end
+end
