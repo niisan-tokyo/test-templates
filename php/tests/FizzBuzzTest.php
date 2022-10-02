951e16ca9c9211eaacbf86e6379f76246999b5dd
niisan-tokyo <niikura23@gmail.com>
2022-10-02 16:07:28 +0900

    PHP: TODOリスト作る

--- php/tests/FizzBuzzTest.php
+++ php/tests/FizzBuzzTest.php
@@ -1,9 +1,80 @@
 <?php
 namespace Tests;
 
+use App\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    
+
+    private FizzBuzz $obj;
+
+    public function setUp(): void
+    {
+        $this->obj = new FizzBuzz;
+    }
+
+    /**
+     * @test
+     */
+    public function 値に1をいれたら1を返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に2を入れたら2を返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に3をいれたらFizzを返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に6をいれたらFizzを返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に5をいれたらBuzzを返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に10をいれたらBuzzを返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に15をいれたらFizzBuzzを返す()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function 値に30をいれたらFizzBuzzを返す()
+    {
+
+    }
 }
