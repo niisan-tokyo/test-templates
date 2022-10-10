4164f62b47791601019ea6953c608367cda6f3a4
niisan-tokyo <niikura23@gmail.com>
2022-10-10 14:43:32 +0900

    fizzbuzz途中まで

---
+++ Rust/fizz_buzz/src/lib.rs
@@ -0,0 +1,51 @@
+pub fn fizzbuzz(num: usize) -> String {
+    num.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn input_1_and_return_1() {
+        let result = fizzbuzz(1);
+        assert_eq!(result, "1");
+    }
+
+    #[test]
+    fn input_2_and_return_2() {
+        let result = fizzbuzz(2);
+        assert_eq!(result, "2");
+    }
+
+    #[test]
+    fn input_3_and_return_fizz() {
+        let result = fizzbuzz(3);
+        assert_eq!(result, "fizz");
+    }
+
+    #[test]
+    fn input_6_and_return_fizz() {
+
+    }
+
+    #[test]
+    fn input_5_and_return_buzz() {
+
+    }
+
+    #[test]
+    fn input_10_and_return_buzz() {
+
+    }
+
+    #[test]
+    fn input_15_and_return_fizzbuzz() {
+
+    }
+
+    #[test]
+    fn input_30_and_return_fizzbuzz() {
+
+    }
+}
